77fe3a9c508559ca869c996cff95e72cd3f88044
Syed Abdul Rehman <syedabdulrehman05@gmail.com>
2022-11-01 12:42:34 +0500

    Updating Links

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -3,19 +3,19 @@ var countDownDate = new Date("Jan 9, 2023 00:00:00").getTime();
 
 var x = setInterval(function() {
 
-  // Get today's date and time
+
   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
+
   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
+
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
+
 
 
   document.getElementById("days").innerHTML = days;
@@ -27,9 +27,10 @@ var x = setInterval(function() {
   document.getElementById("hours-").innerHTML = hours;
   document.getElementById("minutes-").innerHTML = minutes;
   document.getElementById("seconds-").innerHTML = seconds;
-  // If the count down is finished, write some text
+
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("countdown-expired").innerHTML = "EXPIRED";
+    document.getElementById("countdown-expired-").innerHTML = "EXPIRED";
   }
 }, 1000);
